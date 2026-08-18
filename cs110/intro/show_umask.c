526495c7ebd46e1a96343756dbf0b7cff14132af
tunedwtf <293797631+tunedwtf@users.noreply.github.com>
2026-08-18 18:02:17 +0530

    cs110: how to copy

--- cs110/intro/show_umask.c
+++ cs110/intro/show_umask.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
-#include<sys/types.h>
-#include<sys/stat.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 int main() {
-    mode_t old_mask = umask(0);
-    umask(old_mask);
+  mode_t old_mask = umask(0);
+  umask(old_mask);
 
-    printf("umask is set to %03o\n", old_mask);
-    return 0;
+  printf("umask is set to %03o\n", old_mask);
+  return 0;
 }
