3476df1b5490e3c812d2ae4a7c2ba1d83dd8a31c
tunedwtf <293797631+tunedwtf@users.noreply.github.com>
2026-08-21 00:14:27 +0530

    algorithms: array as parameters

--- algorithms/basics/fns.cpp
+++ algorithms/basics/fns.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+// INFO: call by reference should be used carefully
+// INFO: it should not be used on expensive fns.
 void swap(int &x, int &y) {
     int temp;
     temp = x;
@@ -11,12 +13,37 @@ void swap(int &x, int &y) {
     y = temp;
 }
 
+// INFO: Arrays are always pass by address; so here we have n as pass by value and A as address.
+ void print(int A[], int n) {
+     for(int i=0; i < n; i++) {
+         cout<<A[i]<<endl;
+     }
+ }
+
+ int *create(int n) {
+     int *p = (int *)malloc(n);
+     return p;
+ }
+
 int main()
 {
     int a, b;
     a = 10;
     b = 20;
     swap(a, b);
-
     cout<<"a: "<<a<<" b: "<<b<<endl;
+
+
+    int A[5] = {1, 2, 3, 4, 5};
+    print(A, 5);
+
+    int *B = create(5);
+    B[0] = 6;
+    B[1] = 7;
+    B[2] = 8;
+    B[3] = 9;
+    B[4] = 10;
+
+    print(B, 5);
+
 }
