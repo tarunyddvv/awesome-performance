b871bade682249af029e880e39e2dbf0c7b2576a
tunedwtf <293797631+tunedwtf@users.noreply.github.com>
2026-08-21 20:52:35 +0530

    algorithms: factorial of n recursion

---
+++ algorithms/recursion/fact.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int fact(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    return fact(n-1)*n;
+}
+
+int main(){
+    printf("factorial of %d is %d\n", 5, fact(5));
+
+    return 0;
+}
