717235d4df1ed9c91f2f830db4a17f4c3bb55701
tunedwtf <293797631+tunedwtf@users.noreply.github.com>
2026-08-22 03:22:45 +0530

    algorithms: arrays deletion

--- algorithms/arrays/main.c
+++ algorithms/arrays/main.c
@@ -26,13 +26,24 @@ void append(struct Array *ar, int elem) {
     ar->length++;
 }
 
-void insert(struct Array *ar, int ind, int elem) {
-    if (ind>=0 && ind <= ar->length) 
-    {
-        for(int i=ar->length;i>ind;i--)
+void insert(struct Array *ar, int index, int elem) {
+    // check if index > 0 & index < ar.length - 1
+    if (index >= 0 && index < ar->length) {
+        for (int i=ar->length;i>index;i--) {
             ar->a[i] = ar->a[i-1];
-        ar->a[ind]=elem;
+        }
+        ar->a[index] = elem;
         ar->length++;
+    }
+}
+
+void delete(struct Array *ar, int index) {
+    // check if index > 0 & index < ar.length - 1
+    if (index >= 0 && index < ar->length) {
+        for (int i=index;i<ar->length;i++) {
+            ar->a[i] = ar->a[i+1];
+        }
+        ar->length--;
     }
 }
 
@@ -48,8 +59,13 @@ int main()
     append(&ar, 50);
 
     insert(&ar, 2, 45);
-
-
+    printf("before deletion\n");
+    print(&ar);
+    
+    delete(&ar, 2);
+    
+    
+    printf("after deletion\n");
     print(&ar);
 
     return 0;
